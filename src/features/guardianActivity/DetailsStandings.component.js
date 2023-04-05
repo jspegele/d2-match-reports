9b304af5688aa94134f6705c624bf292c84eecd0
Justin Spegele <justin.spegele@gmail.com>
2023-04-05 11:00:27 -0400

    Fixed standings table display issue

--- src/features/guardianActivity/DetailsStandings.component.js
+++ src/features/guardianActivity/DetailsStandings.component.js
@@ -6,19 +6,19 @@ import DetailsStandingsTable from "./DetailsStandingsTable.component"
 
 const DetailsStandings = ({ entries, teams }) => (
     teams.length ? (
-      teams.map((team) => (
-        <Grid container spacing={6}>
-          <Grid item key={team.teamId} xs={12} md={6}>
-            <DetailsStandingsTable
-              players={entries.filter(
-                (entry) => entry.values.team.basic.value === team.teamId
-              )}
-              winner={team.standing.basic.value === 0}
-              pve={false}
-            />
-          </Grid>
-        </Grid>
-      ))
+      <Grid container spacing={6}>
+        {teams.map((team) => (
+            <Grid item key={team.teamId} xs={12} md={6}>
+              <DetailsStandingsTable
+                players={entries.filter(
+                  (entry) => entry.values.team.basic.value === team.teamId
+                )}
+                winner={team.standing.basic.value === 0}
+                pve={false}
+              />
+            </Grid>
+        ))}
+      </Grid>
     ) : (
       <DetailsStandingsTable players={entries} />
     )
